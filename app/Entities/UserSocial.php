f6c7ff5f539db6fe6acf0e94ea1ac3c01a09595f
arlenmendes <arlenmsilva@gmail.com>
2017-11-14 19:54:22 -0200

    adicionando model e migration para controle de dados para cadastro via Social Midia

---
+++ app/Entities/UserSocial.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserSocial extends Model
+{
+    use SoftDeletes;
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'social_network', 'social_id', 'social_email', 'social_avatar'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
+
+
+
+
+
+
