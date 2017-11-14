c3af5e37e82ec46083fec7aff293d50af11da447
arlenmendes <arlenmsilva@gmail.com>
2017-11-14 19:53:36 -0200

    removendo user da raiz app, e configurando o da pasta Entity

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -3,13 +3,33 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model implements Transformable
+class User extends Authenticatable
 {
-    use TransformableTrait;
+    use SoftDeletes;
+    use Notifiable;
 
-    protected $fillable = [];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'cpf', 'name', 'email', 'phone', 'birth', 'gender', 'notes', 'status', 'permission', 'password',
+    ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
+
