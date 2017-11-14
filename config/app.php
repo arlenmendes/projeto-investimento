7d46acfaeda6b0ebe72fc3f72a086aac7d0e2f5f
arlenmendes <arlenmsilva@gmail.com>
2017-11-14 17:28:47 -0200

    adicionando o repository

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Repository l5
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
+
     ],
 
     /*
