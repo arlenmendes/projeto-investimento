8b40ca4ea353eea3ef4cddcbb76dac7fd1fefb37
arlenmendes <arlenmsilva@gmail.com>
2017-11-16 20:25:22 -0200

    adicionando seed para cadastrar um usuario de teste

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Entities\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create([
+            'cpf'           => '11122233344',
+            'name'          => 'João',
+            'email'         => 'joao@sistema.com',
+            'phone'         => '3599999999',
+            'birth'         => '1980-10-01',
+            'gender'        => 'M',
+            'notes'         => 'asdfghjkl',
+            'status'        => 'active',
+//            'permission'    => '',
+            'password'      => bcrypt('123456'),
+        ]);
     }
 }
